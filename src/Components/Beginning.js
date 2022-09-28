38447379814189fac58b6865e89bde455c16947d
Ash Dan <Arvolley01@gmail.com>
2022-09-28 12:19:32 -0400

    figuring things out

--- src/Components/Beginning.js
+++ src/Components/Beginning.js
@@ -1,9 +1,19 @@
 
-const Beginning = () => {
+const Beginning = ({setter}) => {
+
+    const handleOne = () => {
+        setter(<p>one</p>)
+    }
+    const handleTwo = () => {
+        setter(<p>two</p>)
+    }
 
     return (
         <div>
             <p>Beginning</p>
+            <p>To play this game, simply follow the instructions.</p>
+            <button onClick = {handleOne}>Hi</button>
+            <button onClick={handleTwo}>Bye</button>
         </div>
     )
 }
